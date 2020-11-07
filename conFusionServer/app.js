61804a30e35e2f70d6a918f49c5ccaa2dd15ccd3
evangel <evangeldaping@gmail.com>
2020-11-06 21:47:51 -0800

    File Upload

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -9,6 +9,8 @@ var passport = require('passport');
 var authenticate = require('./authenticate');
 var config = require('./config');
 
+const uploadRouter = require('./routes/uploadRouter');
+
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
@@ -58,6 +60,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
